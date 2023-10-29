f2a08df775c25052436e31978542eca2a22c6e5d
Deborah <deborahvillalba@outlook.com>
2023-10-29 00:40:01 -0300

    froma de pago

---
+++ js/sell.js
@@ -0,0 +1,231 @@
+let productCost = 0;
+let productCount = 0;
+let comissionPercentage = 0.13;
+let MONEY_SYMBOL = "$";
+let DOLLAR_CURRENCY = "Dólares (USD)";
+let PESO_CURRENCY = "Pesos Uruguayos (UYU)";
+let DOLLAR_SYMBOL = "USD ";
+let PESO_SYMBOL = "UYU ";
+let PERCENTAGE_SYMBOL = '%';
+let MSG = "FUNCIONALIDAD NO IMPLEMENTADA";
+
+//Función que se utiliza para actualizar los costos de publicación
+function updateTotalCosts(){
+    let unitProductCostHTML = document.getElementById("productCostText");
+    let comissionCostHTML = document.getElementById("comissionText");
+    let totalCostHTML = document.getElementById("totalCostText");
+
+    let unitCostToShow = MONEY_SYMBOL + productCost;
+    let comissionToShow = Math.round((comissionPercentage * 100)) + PERCENTAGE_SYMBOL;
+    let totalCostToShow = MONEY_SYMBOL + ((Math.round(productCost * comissionPercentage * 100) / 100) + parseInt(productCost));
+
+    unitProductCostHTML.innerHTML = unitCostToShow;
+    comissionCostHTML.innerHTML = comissionToShow;
+    totalCostHTML.innerHTML = totalCostToShow;
+}
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("productCountInput").addEventListener("change", function(){
+        productCount = this.value;
+        updateTotalCosts();
+    });
+
+    document.getElementById("productCostInput").addEventListener("change", function(){
+        productCost = this.value;
+        updateTotalCosts();
+    });
+
+    document.getElementById("goldradio").addEventListener("change", function(){
+        comissionPercentage = 0.13;
+        updateTotalCosts();
+    });
+    
+    document.getElementById("premiumradio").addEventListener("change", function(){
+        comissionPercentage = 0.07;
+        updateTotalCosts();
+    });
+
+    document.getElementById("standardradio").addEventListener("change", function(){
+        comissionPercentage = 0.03;
+        updateTotalCosts();
+    });
+
+    document.getElementById("productCurrency").addEventListener("change", function(){
+        if (this.value == DOLLAR_CURRENCY)
+        {
+            MONEY_SYMBOL = DOLLAR_SYMBOL;
+        } 
+        else if (this.value == PESO_CURRENCY)
+        {
+            MONEY_SYMBOL = PESO_SYMBOL;
+        }
+
+        updateTotalCosts();
+    });
+
+
+    //Configuraciones para el elemento que sube archivos
+    let dzoptions = {
+        url:"/",
+        autoQueue: false
+    };
+    let myDropzone = new Dropzone("div#file-upload", dzoptions);    
+
+
+    //Se obtiene el formulario de publicación de producto
+    let sellForm = document.getElementById("sell-info");
+
+    //Se agrega una escucha en el evento 'submit' que será
+    //lanzado por el formulario cuando se seleccione 'Vender'.
+    sellForm.addEventListener("submit", function(e){
+
+        e.preventDefault(); 
+        e.preventDefault();
+
+        let productNameInput = document.getElementById("productName");
+        let productCategory = document.getElementById("productCategory");
+        let productCost = document.getElementById("productCostInput");
+        let infoMissing = false;
+
+        //Quito las clases que marcan como inválidos
+        productNameInput.classList.remove('is-invalid');
+        productCategory.classList.remove('is-invalid');
+        productCost.classList.remove('is-invalid');
+
+        //Se realizan los controles necesarios,
+        //En este caso se controla que se haya ingresado el nombre y categoría.
+        //Consulto por el nombre del producto
+        if (productNameInput.value === "")
+        {
+            productNameInput.classList.add('is-invalid');
+            infoMissing = true;
+        }
+        
+        //Consulto por la categoría del producto
+        if (productCategory.value === "")
+        {
+            productCategory.classList.add('is-invalid');
+            infoMissing = true;
+        }
+
+        //Consulto por el costo
+        if (productCost.value <=0)
+        {
+            productCost.classList.add('is-invalid');
+            infoMissing = true;
+        }
+        
+        if(!infoMissing)
+        {
+            //Aquí ingresa si pasó los controles, irá a enviar
+            //la solicitud para crear la publicación.
+
+            getJSONData(PUBLISH_PRODUCT_URL).then(function(resultObj){
+                let msgToShowHTML = document.getElementById("resultSpan");
+                let msgToShow = "";
+    
+                //Si la publicación fue exitosa, devolverá mensaje de éxito,
+                //de lo contrario, devolverá mensaje de error.
+                //FUNCIONALIDAD NO IMPLEMENTADA
+                if (resultObj.status === 'ok')
+                {
+                    msgToShow = MSG;
+                    document.getElementById("alertResult").classList.add('alert-primary');
+                }
+                else if (resultObj.status === 'error')
+                {
+                    msgToShow = MSG;
+                    document.getElementById("alertResult").classList.add('alert-primary');
+                }
+    
+                msgToShowHTML.innerHTML = msgToShow;
+                document.getElementById("alertResult").classList.add("show");
+            });
+        }
+    });
+});
+
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2023 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+
+(() => {
+    'use strict';
+  
+    const getStoredTheme = () => localStorage.getItem('theme');
+    const setStoredTheme = theme => localStorage.setItem('theme', theme);
+  
+    const getPreferredTheme = () => {
+      const storedTheme = getStoredTheme();
+      if (storedTheme) {
+        return storedTheme;
+      }
+  
+      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    }
+  
+    const setTheme = theme => {
+      if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+      } else {
+        document.documentElement.setAttribute('data-bs-theme', theme);
+      }
+    }
+  
+    setTheme(getPreferredTheme());
+  
+    const showActiveTheme = (theme, focus = false) => {
+      const themeSwitcher = document.querySelector('#bd-theme');
+  
+      if (!themeSwitcher) {
+        return;
+      }
+  
+      const themeSwitcherText = document.querySelector('#bd-theme-text');
+      const activeThemeIcon = document.querySelector('.theme-icon-active use');
+      const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
+      const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href');
+  
+      document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+        element.classList.remove('active');
+        element.setAttribute('aria-pressed', 'false');
+      });
+  
+      btnToActive.classList.add('active');
+      btnToActive.setAttribute('aria-pressed', 'true');
+      activeThemeIcon.setAttribute('href', svgOfActiveBtn);
+      const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+      themeSwitcher.setAttribute('aria-label', themeSwitcherLabel);
+  
+      if (focus) {
+        themeSwitcher.focus();
+      }
+    }
+  
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+      const storedTheme = getStoredTheme();
+      if (storedTheme !== 'light' && storedTheme !== 'dark') {
+        setTheme(getPreferredTheme());
+      }
+    });
+  
+    window.addEventListener('DOMContentLoaded', () => {
+      showActiveTheme(getPreferredTheme());
+  
+      document.querySelectorAll('[data-bs-theme-value]').forEach(toggle => {
+        toggle.addEventListener('click', () => {
+          const theme = toggle.getAttribute('data-bs-theme-value');
+          setStoredTheme(theme);
+          setTheme(theme);
+          showActiveTheme(theme, true);
+        });
+      });
+    });
+  })();
+  
+  /*Aca termina dark and light*/
